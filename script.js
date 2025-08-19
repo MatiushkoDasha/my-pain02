9b9e963b2939821eedf54e8801c40285a5f42bae
MatiushkoDasha <zmiyka47gmail.com>
2025-08-19 16:38:33 +0300

    media

---
+++ script.js
@@ -0,0 +1,17 @@
+const dark = document.querySelector(".dark-bgc"),
+      burger = document.querySelector(".burger"),
+      center = document.querySelector(".center"),
+      cancel = document.querySelector(".cancel")
+
+burger.addEventListener("click", function() {
+  center.style.left = "0";
+  dark.style.display = "block"
+})
+
+function cancelBurger() {
+  center.style.left = "-100%";
+  dark.style.display = "none"
+}
+cancel.addEventListener("click", cancelBurger)
+dark.addEventListener("click", cancelBurger)
+
