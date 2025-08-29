5d8fe158f72bd0f5053e7492df41ab1bd3e134f2
MatiushkoDasha <zmiyka47gmail.com>
2025-08-29 12:26:05 +0300

    media13

--- java/script.js
+++ java/script.js
@@ -1,15 +1,15 @@
 const dark = document.querySelector(".dark-bgc"),
       burger = document.querySelector(".burger"),
-      center = document.querySelector(".center")
+      menu = document.querySelector(".menu")
       // cancenter
 burger.addEventListener("click", function() {
-  center.style.left = "0";
+  menu.style.right = "0";
   dark.style.display = "block"
 })
 function cancelBurger() {
-  center.style.left = "-100%";
+  menu.style.right = "-100%";
   dark.style.display = "none"
 }
-// cancel.addEventListener("click", cancelBurger)
+//cancel.addEventListener("click", cancelBurger)
 dark.addEventListener("click", cancelBurger)
-
+c
